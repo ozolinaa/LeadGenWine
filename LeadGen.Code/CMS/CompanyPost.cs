3fc34d5074964d5a24fec81191f84d5f32069b41
Anton Ozolin <anton.ozolin@gmail.com>
2020-01-24 00:29:42 -0800

    SyncCRM in progress

---
+++ LeadGen.Code/CMS/CompanyPost.cs
@@ -0,0 +1,67 @@
+﻿using LeadGen.Code.Map;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+
+namespace LeadGen.Code.CMS
+{
+	public class CompanyPost : Post
+	{
+		public CompanyPost() : base() { }
+		public CompanyPost(DataRow row) : base(row) { }
+
+		public string company_web_site_official { 
+			get { return getFieldByCode("company_web_site_official").fieldText; } 
+			set { getFieldByCode("company_web_site_official").fieldText = value; } 
+		}
+
+		public string company_web_site_other
+		{
+			get { return getFieldByCode("company_web_site_other").fieldText; }
+			set { getFieldByCode("company_web_site_other").fieldText = value; }
+		}
+
+		public string company_public_email
+		{
+			get { return getFieldByCode("company_public_email").fieldText; }
+			set { getFieldByCode("company_public_email").fieldText = value; }
+		}
+		public long? company_notification_phone
+		{
+			get { return getFieldByCode("company_notification_phone").fieldNumber; }
+			set { getFieldByCode("company_notification_phone").fieldNumber = value; }
+		}
+		public long? company_public_phone
+		{
+			get { return getFieldByCode("company_public_phone").fieldNumber; }
+			set { getFieldByCode("company_public_phone").fieldNumber = value; }
+		}
+		public string company_notification_email
+		{
+			get { return getFieldByCode("company_notification_email").fieldText; }
+			set { getFieldByCode("company_notification_email").fieldText = value; }
+		}
+		public Location company_notification_location
+		{
+			get { return getFieldByCode("company_notification_location").location; }
+			set { getFieldByCode("company_notification_location").location = value; }
+		}
+		public bool company_notification_do_not_send_leads
+		{
+			get { return getFieldByCode("company_notification_do_not_send_leads").fieldBool; }
+			set { getFieldByCode("company_notification_do_not_send_leads").fieldBool = value; }
+		}
+		public long? company_businessId
+		{
+			get { return getFieldByCode("company_businessId").fieldNumber; }
+			set { getFieldByCode("company_businessId").fieldNumber = value; }
+		}
+		public string company_crmId
+		{
+			get { return getFieldByCode("company_crmId").fieldText; }
+			set { getFieldByCode("company_crmId").fieldText = value; }
+		}
+	}
+}
--- LeadGen.Code/Sys/Scheduled/SyncCRM.cs
+++ LeadGen.Code/Sys/Scheduled/SyncCRM.cs
@@ -1,4 +1,5 @@
 ﻿using LeadGen.Code.Clients.CRM;
+using LeadGen.Code.CMS;
 using LeadGen.Code.Lead.Notification;
 using System;
 using System.Collections.Generic;
@@ -11,15 +12,52 @@ namespace LeadGen.Code.Sys.Scheduled
 {
     public class SyncCRM : ScheduledTask
     {
+        private List<Post> companyPosts = null;
+        private List<Organization> crmOrgs = null;
+
         protected override string RunInternal(SqlConnection con)
         {
-            string mysqlString = Helpers.SysHelper.AppSettings.CRMSettings.DBConnectionString;
-            ESPOClient client = new ESPOClient(mysqlString);
-            List<Organization> orgs = client.GetOrganizations();
-            Organization org = orgs.FirstOrDefault(x => x.Name == "XtonyX Rambler TEST");
-            client.SetBusinessID(org.ID, null);
-            client.SetPostID(org.ID, null);
-            client.OptOutEmailLeadNotifications(org.ID, false);
+            //string mysqlString = Helpers.SysHelper.AppSettings.CRMSettings.DBConnectionString;
+            //ESPOClient client = new ESPOClient(mysqlString);
+            //crmOrgs = client.GetOrganizations();
+
+            string orgId = "5e216391071e53d68";
+            string orgName = "XTONYX";
+
+            CompanyPost companyPost = Post.SelectFromDB<CompanyPost>(con, fieldCode: "company_crmId", textValue: orgId).FirstOrDefault();
+            if (companyPost == null)
+            {
+                long postId = 10011; // Post.CreateNew(con, 1, PostType.BuiltIn.Company);
+                companyPost = Post.SelectFromDB<CompanyPost>(con, postID: postId).First();
+                companyPost.postStatus = new Post.Status() { ID = 30 }; // Pending
+            }
+            companyPost.LoadFields(con);
+            companyPost.company_crmId = orgId;
+            companyPost.title = orgName;
+
+            string errorMessage = null;
+            companyPost.Update(con, ref errorMessage);
+            if (!string.IsNullOrEmpty(errorMessage))
+                throw new Exception("Error updating company post during sync CRM");
+
+            //companyPosts = Post.SelectFromDB<Post>(con, fieldCode: "ff", po).ToList();
+
+            //foreach (Organization org in client.GetOrganizations())
+            //{
+            //    total++;
+
+            //    if (org.LeadGenPostID == null)
+            //    {
+            //        if(CMS.Post.SelectFromDB<Post>(con,)
+            //    }
+            //        CMS.Post.CreateNew(con, 1, CMS.PostType.BuiltIn.Company)
+
+            //}
+
+            //Organization org = orgs.FirstOrDefault(x => x.Name == "XtonyX Rambler TEST");
+            //client.SetBusinessID(org.ID, null);
+            //client.SetPostID(org.ID, null);
+            //client.OptOutEmailLeadNotifications(org.ID, false);
 
             return string.Format("Companies Synced With CRM: {0}", 3);
         }
