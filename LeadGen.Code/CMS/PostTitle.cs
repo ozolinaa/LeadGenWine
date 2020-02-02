52c03f6a8b1bd695b5f9f5231dfc773044f648ce
Anton Ozolin <anton.ozolin@gmail.com>
2020-02-01 23:38:55 -0800

    post title

---
+++ LeadGen.Code/CMS/PostTitle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeadGen.Code.CMS
+{
+    public class PostTitle
+    {
+        public PostTitle(Post post)
+        {
+            Title = post.title;
+        }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public bool ShowSiteName = false;
+        public bool ShowBorder { get; set; }
+    }
+}
