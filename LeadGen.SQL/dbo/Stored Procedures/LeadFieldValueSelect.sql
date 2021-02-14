74790004277e787175594509ef4454894657fb4b
Anton Ozolin <anton.ozolin@gmail.com>
2021-02-14 11:33:53 -0800

    added _system_location_address field

--- LeadGen.SQL/dbo/Stored Procedures/LeadFieldValueSelect.sql
+++ LeadGen.SQL/dbo/Stored Procedures/LeadFieldValueSelect.sql
@@ -12,15 +12,11 @@ BEGIN
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
-	SELECT L.LeadID, LF.FieldID, LF.FieldCode, LF.FieldName, LF.LabelText, LF.FieldTypeID, LFT.FieldTypeName, 
+	SELECT L.LeadID, LF.FieldID, LF.[Order], LF.FieldCode, LF.FieldName, LF.LabelText, LF.FieldTypeID, LFT.FieldTypeName, 
 	LF.IsRequired, LF.IsContact, LF.IsActive, LF.[Description],
 	LFG.GroupID, LFG.GroupCode, LFG.GroupTitle,
 	FVS.TextValue, FVS.DatetimeValue, FVS.BoolValue, FVS.NumberValue, 
-	TT.TermID,
-	TT.TermURL,
-	TT.TermName,
-	TT.TermThumbnailURL,
-	TT.TaxonomyID
+	TT.TermID, TT.TermURL, TT.TermName, TT.TermThumbnailURL, TT.TaxonomyID
 	FROM [dbo].[Lead] L
 	INNER JOIN @LeadIDTable LT ON LT.Item = L.LeadID
 	CROSS JOIN [dbo].[LeadFieldStructure] LF
@@ -29,6 +25,23 @@ BEGIN
 	LEFT OUTER JOIN [dbo].[LeadFieldValueScalar] FVS ON FVS.[LeadID] = L.[LeadID] AND FVS.[FieldID] = LF.[FieldID]
 	LEFT OUTER JOIN [dbo].[LeadFieldValueTaxonomy] FVT ON FVT.[LeadID] = L.[LeadID] AND LF.[FieldID] = FVT.[FieldID]
 	LEFT OUTER JOIN [dbo].[TaxonomyTerm] TT ON TT.TermID = FVT.TermID
-	ORDER BY L.LeadID, LFG.GroupID, LF.[Order]
+	UNION ALL
+	SELECT 
+	LL.LeadID, -1 as FieldID, -1 as [Order], '_system_location_address' as [FieldCode], 'Address' as [FieldName], 'Address' as [LabelText], LFT.[FieldTypeID], LFT.[FieldTypeName], 
+	0 as [IsRequired], 0 as [IsContact], 1 as [IsActive], '' as [Description],
+	-1 as [LFG.GroupID], '_system' as [GroupCode], 'System' as [GroupTitle],
+	StreetAddress as TextValue, NULL as DatetimeValue, NULL as BoolValue, NULL as NumberValue,
+	NULL as [TermID], NULL as [TermURL], NULL as [TermName], NULL as [TermThumbnailURL], NULL as [TaxonomyID]
+	FROM @LeadIDTable as L
+	LEFT OUTER JOIN 
+	( 
+	 SELECT LeadID, MAX(LocationID) as LocationID, -1 as [Order]
+	 FROM LeadLocation
+	 GROUP BY LeadID -- DB schema allows to have multiple locations per lead, but business logic not
+	) LL ON LL.LeadID = L.Item
+	LEFT OUTER JOIN [dbo].[Location] LOC ON LOC.LocationID = LL.LocationID
+	LEFT OUTER JOIN [dbo].[LeadFieldType] LFT ON LFT.FieldTypeID = 1 --Textbox
+
+	ORDER BY [LeadID], [GroupID], [Order]
 
 END
